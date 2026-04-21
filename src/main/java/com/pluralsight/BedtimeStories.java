a673ebcf28ad6c5e25737b8840f3b7bc6b2995b2
Dollant <ariawilliams2425@gmail.com>
2026-04-21 11:28:04 -0400

    Added while and try catch to have system catch any misinputs and reprompt until a valid choice is selected.

--- src/main/java/com/pluralsight/BedtimeStories.java
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -16,21 +16,35 @@ public class BedtimeStories {
         System.out.println("1. Goldilocks and the Three Bears");
         System.out.println("2. Hansel and Gretel");
         System.out.println("3. Mary Had a Little Lamb");
-        System.out.print("\nPick a story (1-3): ");
 
-        String choice = keyboard.nextLine().trim();
+        String fileName = null;
 
-        String fileName;
-        switch (choice) {
-            case "1":
-                fileName = "src/main/resources/goldilocks.txt";
-                break;
-            case "2":
-                fileName = "src/main/resources/hansel_and_gretel.txt";
-                break;
-            case "3":
-                fileName = "src/main/resources/mary_had_a_little_lamb.txt";
-                break;
+        while (fileName == null) {
+
+            System.out.print("\nPick a story (1-3): ");
+            String choice = keyboard.nextLine().trim();
+
+            try {
+                switch (choice) {
+                    case "1":
+                        fileName = "src/main/resources/goldilocks.txt";
+                        break;
+                    case "2":
+                        fileName = "src/main/resources/hansel_and_gretel.txt";
+                        break;
+                    case "3":
+                        fileName = "src/main/resources/mary_had_a_little_lamb.txt";
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid Choice: " + choice);
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("Oopsie! " + e.getMessage() + ". Please pick 1, 2, or 3. Try again!");
+
+            }
         }
+
     }
-}
+
+        }
+
