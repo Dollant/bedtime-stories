6522a105735b201050a87b8d35c7ae92e94c337d
Dollant <ariawilliams2425@gmail.com>
2026-04-21 11:00:02 -0400

    Scanner active.

--- src/main/java/com/pluralsight/BedtimeStories.java
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class BedtimeStories {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+    }
 }
